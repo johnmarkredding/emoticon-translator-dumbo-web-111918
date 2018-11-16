bf5e5ade6076eaec3e93938eecfca5aeef7625bd
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 22:12:22 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,7 @@ def load_library(path)
   emos = YAML.load_file(path)
   
   emos.each || do |k_word,v_arr|
-    new_hash["get_meaning"][v_arr[1]] = emos
+    new_hash["get_meaning"][v_arr[1]] = k_word
   end
   
   new_hash["get_emoticon"] = emos.invert
