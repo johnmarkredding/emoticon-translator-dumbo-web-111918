43243025ca26267eb64342cbe5a43a63f074ec23
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 22:05:03 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,7 +3,7 @@ require 'yaml'
 def load_library(path)
   new_hash = {}
   emos = YAML.load_file(path)
-  new_hash["get_meaning"] = emos
+  new_hash["get_meaning"] = emos.invert
 end
 
 def get_japanese_emoticon
