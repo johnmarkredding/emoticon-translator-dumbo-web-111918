cf367cb0a60299fb4c2239942d5347a214274a54
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 22:12:48 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,6 +9,7 @@ def load_library(path)
   
   emos.each || do |k_word,v_arr|
     new_hash["get_meaning"][v_arr[1]] = k_word
+    new_hash["get_emoticon"][v_arr[0]] = k_word
   end
   
   new_hash["get_emoticon"] = emos.invert
