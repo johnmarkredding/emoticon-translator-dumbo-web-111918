d0715b4902bbc1fa994aff9a23c5d01ad707e3eb
John Mark Redding <johnmarkredding@gmail.com>
2018-11-16 22:01:29 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,16 +1,12 @@
-# require modules here
 require 'yaml'
 
 def load_library(path)
-  # code goes here
   emos = YAML.load_file(path)
   
 end
 
 def get_japanese_emoticon
-  # code goes here
 end
 
 def get_english_meaning
-  # code goes here
 end
